449d3373027274a863c8b50d57b124b49e8b5cec
khanhkimtran <107995007+khanhkimtran@users.noreply.github.com>
2022-08-24 00:47:49 +1000

    Add files via upload

---
+++ convertToSheets/Main.py
@@ -0,0 +1,38 @@
+# Python program to read
+# json file
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import json
+import pandas as pd
+
+
+def convertFileToSheet():
+    
+    scope =["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+
+    creds = ServiceAccountCredentials.from_json_keyfile_name("tasksheets-360312-bbf002405e77.json", scope)
+
+    employee = gspread.authorize(creds)
+
+    sheet = employee.open('Sight Data Employee').sheet1
+    
+    file = open('Sample-employee-JSON-data.json')
+    data = json.load(file)
+    df = pd.DataFrame.from_dict(pd.json_normalize(data), orient='columns')
+    #wks = sheet[0]
+    # .from_dict(pd.json_normalize(data), orient='columns')
+
+    #update the first sheet with df, starting at cell B2. 
+    sheet.set_dataframe(df,(1,1))
+    
+    # for i in data['Employees']:
+    #     print(i)
+        #sheet.insert_row(i, headers={'Content-Type': 'application/json'})
+        #sheet.insert_row(row,1)
+
+def main():
+    convertFileToSheet()
+
+if __name__ == "__main__":
+    main()
