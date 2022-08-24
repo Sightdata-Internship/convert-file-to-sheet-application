d59742177203bf5807c6867080202a353bd14804
khanhkimtran <107995007+khanhkimtran@users.noreply.github.com>
2022-08-24 16:25:30 +1000

    Add files via upload

--- convertToSheets/Main.py
+++ convertToSheets/Main.py
@@ -1,33 +1,32 @@
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
 import json
 import pandas as pd
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import gspread_dataframe as gd
+
 
 # function: convertFileToSheet
 ## reads and convert JSON file to google sheet
 def convertFileToSheet():
     
+    #connect Python with Google Sheet using API keys
     scope =["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
-
     creds = ServiceAccountCredentials.from_json_keyfile_name("tasksheets-360312-bbf002405e77.json", scope)
-
     employee = gspread.authorize(creds)
-
     sheet = employee.open('Sight Data Employee').sheet1
     
+    #read JSON file into dataframe 
     file = open('Sample-employee-JSON-data.json')
     data = json.load(file)
     df = pd.DataFrame.from_dict(pd.json_normalize(data), orient='columns')
-    #wks = sheet[0]
-    # .from_dict(pd.json_normalize(data), orient='columns')
-
-    #update the first sheet with df, starting at cell B2. 
-    sheet.set_dataframe(df,(1,1))
     
-    # for i in data['Employees']:
-    #     print(i)
-        #sheet.insert_row(i, headers={'Content-Type': 'application/json'})
-        #sheet.insert_row(row,1)
+    #insert JSON file into sheet as a dataframe
+    for i in df['Employees']:
+        existing = gd.get_as_dataframe(sheet)
+        updated = existing.append(i)
+        gd.set_with_dataframe(sheet, updated)
+    # headers={'Content-Type': 'application/json'}
 
 def main():
     convertFileToSheet()
