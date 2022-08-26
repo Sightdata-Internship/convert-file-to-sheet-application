0bb2d7abdafbf1246645f5993b7625adb604608c
khanhkimtran <107995007+khanhkimtran@users.noreply.github.com>
2022-08-27 01:15:55 +1000

    Add files via upload

--- convertToSheets/Main.py
+++ convertToSheets/Main.py
@@ -4,7 +4,7 @@ import pandas as pd
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import gspread_dataframe as gd
-from gspread_dataframe import set_with_dataframe
+from gspread_dataframe import set_with_dataframe, get_as_dataframe
 
 # function: convertFileToSheet
 ## reads and convert JSON file to google sheet
@@ -26,11 +26,35 @@ def convertFileToSheet():
         df = pd.DataFrame.from_dict(pd.json_normalize(i), orient='columns')
         existing = existing.append(df)
     
+    #add dataframe to Google Sheet 
+    set_with_dataframe(sheet, existing)  
+    return sheet
+
+# function: addFileToSheet
+## reads and convert other JSON files to a available google sheet
+def addFileToSheet(sheet):
+    #read another JSON file into dataframe
+    file = open('Employee-JSON-data-2.json')
+    data2 = json.load(file)
+    
+    #get the value available in Google Sheet
+    df1 = pd.DataFrame(sheet.get_all_records())
+    
+    #add all employee info into an existing dataframe
+    existing = pd.DataFrame()
+    for i in data2['Employees']:
+        df = pd.DataFrame.from_dict(pd.json_normalize(i), orient='columns')
+        existing = existing.append(df)
+    
+    #combine values from new file and Google Sheet as dataframe
+    existing = existing.append(df1)
+    
     #add dataframe to Google Sheet    
-    set_with_dataframe(sheet, existing)    
+    set_with_dataframe(sheet, existing)
     
 def main():
-    convertFileToSheet()
+    sheet = convertFileToSheet()
+    addFileToSheet(sheet)
 
 if __name__ == "__main__":
     main()
